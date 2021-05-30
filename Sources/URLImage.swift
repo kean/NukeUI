b489310c53185c049cea677c76caa8b9d92f4905
kean <grebenyuk.alexander@gmail.com>
2021-05-29 20:57:21 -0400

    Add URLImage

--- Sources/URLImage.swift
+++ Sources/URLImage.swift
@@ -3,3 +3,12 @@
 // Copyright (c) 2015-2021 Alexander Grebenyuk (github.com/kean).
 
 import Foundation
+import Nuke
+import SwiftUI
+
+@available(iOS 14.0, tvOS 14.0, watchOS 7.0, macOS 11.0, *)
+public struct URLImage: View {
+    public var body: some View {
+        Text("Placeholder")
+    }
+}
