9218b910df1e56eb472d35e3f1a5572216b58ba1
kean <grebenyuk.alexander@gmail.com>
2021-05-30 18:29:45 -0400

    Update

--- Sources/LazyImage.swift
+++ Sources/LazyImage.swift
@@ -167,8 +167,6 @@ public struct LazyImage: View {
     }
 }
 
-#warning("add onSuccess/onFailure callbacks")
-
 #if os(macOS)
 @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
 private struct LazyImageViewWrapper: NSViewRepresentable {
