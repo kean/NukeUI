3d04141e98a1ca635d40d88bc4cfc7ed11b67485
kean <grebenyuk.alexander@gmail.com>
2021-05-30 09:44:16 -0400

    Add configure closure

--- Sources/URLImage.swift
+++ Sources/URLImage.swift
@@ -12,6 +12,7 @@ public struct URLImage: View {
     private var placeholder: AnyView?
     private let source: ImageRequestConvertible?
     @StateObject private var model = URLImageViewModel()
+    private var configure: ((URLImageView) -> Void)?
 
     public init(source: ImageRequestConvertible?) {
         self.source = source
@@ -19,19 +20,31 @@ public struct URLImage: View {
 
     #warning("impl")
     /// Displayed while the image is loading.
-    public func placeholder<Placeholder: View>(_ view: Placeholder?) -> URLImage {
+    public func placeholder<Placeholder: View>(_ view: Placeholder?) -> Self {
         var copy = self
         copy.placeholder = placeholder
-        return self
+        return copy
     }
 
     #warning("options to customize image view")
 
+    /// Returns an underlying image view.
+    ///
+    /// - parameter configure: A closure that gets called once when the view is
+    /// created and allows you to configure it based on your needs.
+    public func imageView(_ configure: @escaping (URLImageView) -> Void) -> Self {
+        var copy = self
+        copy.configure = configure
+        return copy
+    }
+
     public var body: some View {
-        URLImageViewWrapper(model: model)
+        URLImageViewWrapper(model: model, configure: configure)
             .onAppear { model.source = source }
     }
 }
+
+#warning("add onSuccess/onFailure callbacks")
 
 @available(iOS 14.0, tvOS 14.0, watchOS 7.0, macOS 11.0, *)
 private final class URLImageViewModel: ObservableObject {
@@ -41,9 +54,12 @@ private final class URLImageViewModel: ObservableObject {
 @available(iOS 14.0, tvOS 14.0, watchOS 7.0, macOS 11.0, *)
 private struct URLImageViewWrapper: UIViewRepresentable {
     @ObservedObject var model: URLImageViewModel
+    var configure: ((URLImageView) -> Void)?
 
     func makeUIView(context: Context) -> URLImageView {
-        URLImageView()
+        let view = URLImageView()
+        configure?(view)
+        return view
     }
 
     func updateUIView(_ imageView: URLImageView, context: Context) {
