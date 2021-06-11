3255738aec56fa60606f9fedf3c207328c8bc56c
kean <grebenyuk.alexander@gmail.com>
2021-06-11 13:45:53 -0400

    Remove unused withoutAnimation

--- Sources/Internal.swift
+++ Sources/Internal.swift
@@ -109,10 +109,3 @@ extension ImageRequest {
         }
     }
 }
-
-@available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
-func withoutAnimation(_ closure: () -> Void) {
-    var transaction = Transaction(animation: nil)
-    transaction.disablesAnimations = true
-    withTransaction(transaction, closure)
-}
